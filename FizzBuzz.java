b47a7bafb4abd92ac2ec3ff22ca57195c4726728
Shaun Miller <shaunmiller236@gmail.com>
2017-03-03 22:53:47 -0700

    Added business logic.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,5 +7,32 @@ public class FizzBuzz
     {
         System.out.println("Hello World");
         System.out.println("I am ready!");
+
+        System.out.println("Please input an integer value.  Robot will count down from integer value to 0!");
+        System.out.println("Input value on next line:");
+
+        //TODO: User input, but I only have 5 more minute or so, just roll with a standard integer for now
+        int countdown = 25;
+        while (countdown >= 0)
+        {
+            if ((countdown % 3 == 0) && (countdown % 5 == 0))
+            {
+                System.out.println("FIZZBUZZ!!!");
+            }
+            else if (countdown % 5 == 0)
+            {
+                System.out.println("BUZZ!!");
+            }
+            else if (countdown % 3 == 0)
+            {
+                System.out.println("FIZZ!");
+            }
+            else
+            {
+                System.out.println(countdown);
+            }
+            countdown--;
+        }
+
     }
 }
